216268175387d26c01b578f4848cea6752848071
John Soklaski <jsokla@microsoft.com>
2016-09-07 11:21:15 -0700

    Change email to use benchview alias

--- rolling-perf/rolling-perf.py
+++ rolling-perf/rolling-perf.py
@@ -359,7 +359,7 @@ def upload_to_benchview(commit, timestamp, branch, xunit_xml_path):
     with PushDir(job_benchview_dir):
         RunCommand(['py', os.path.join(benchview_tools_dir, 'submission-metadata.py'),
                                                                         '--name', 'dotnet cli rolling perf {}'.format(commit),
-                                                                        '--user-email', 'johndoe@microsoft.com'
+                                                                        '--user-email', 'benchview@microsoft.com'
                                                                         ])
         RunCommand(['py', os.path.join(benchview_tools_dir, 'build.py'),
                                                                         '--branch', branch,
